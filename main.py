d07e5711dea3dd3a6554f148525822035e380793
Sourcery AI <>
2022-09-26 17:01:02 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ while True:
     output = face_mesh.process(rgb_frame)  # Process the frame
     landmark_points = output.multi_face_landmarks  # Landmark points
     frame_h, frame_w, _ = frame.shape  # Frame size
-    
+
     if landmark_points:  # If there are landmark points
         landmarks = landmark_points[0].landmark  # Landmarks
         for i, landmark in enumerate(landmarks[474:478]):  # For each landmark
@@ -29,16 +29,16 @@ while True:
                 screen_x = screen_w * landmark.x  # X coordinate
                 screen_y = screen_h * landmark.y  # Y coordinate
                 pyautogui.moveTo(screen_x, screen_y)  # Move the cursor
-        
+
         left = [landmarks[145], landmarks[159]]  # Left eye
         for landmark in left:  # For each landmark
             x = int(landmark.x * frame_w)  # X coordinate
             y = int(landmark.y * frame_h)  # Y coordinate
             cv2.circle(frame, (x, y), 3, (0, 255, 255))  # Draw a circle
-        
+
         if (left[0].y - left[1].y) < 0.004:  # If the left eye is closed
             pyautogui.click()  # Click
             pyautogui.sleep(1)  # Wait for 1 second
-    
+
     cv2.imshow("Meyes ", frame)  # Show the frame
     cv2.waitKey(1)  # Wait for 1 millisecond
